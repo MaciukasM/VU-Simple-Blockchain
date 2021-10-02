c98bd28b7769d38db865024b25de4dbace9f3491
Mantas Mačiukas <mantas.masiukas@gmail.com>
2021-10-02 15:19:12 +0300

    Fixing random value+pk generation

--- funkcijos.cpp
+++ funkcijos.cpp
@@ -3,15 +3,17 @@
 
 void VartotojuGeneravimas(vector<User> &vartotojai)
 {
+    random_device device;
+    mt19937 generator(device());
+    uniform_int_distribution<int> distribution(100,1000000);
+
     for(int i = 0; i<1000; i++)
     {
-        srand(time(NULL));
-
         User u;
 
         u.setVardas("Vardas"+to_string(i));
-        u.setPk(DuomenuHashinimas(u.getVardas())); //laikinas private key generavimo sprendimas
-        u.setVal(rand()%999900+100);
+        u.setVal(distribution(generator));
+        u.setPk(DuomenuHashinimas(u.getVardas()+to_string(u.getVal()))); //laikinas private key generavimo sprendimas
 
         vartotojai.push_back(u);
     }
